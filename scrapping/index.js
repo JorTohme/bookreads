b9b4dcf11f776bb5df53bfe38132058965b8855d
Jorge <jorlopeztohme@gmail.com>
2023-01-10 19:02:18 -0300

    Scrapping test

--- scrapping/index.js
+++ scrapping/index.js
@@ -1,7 +1,10 @@
 import * as cheerio from 'cheerio'
 
+import { writeFile } from 'node:fs/promises'
+import path from 'node:path'
+
 // Books array
-const books = [{}]
+const books = []
 
 // Scrapping
 const res = await fetch('https://www.bookdepository.com/es/')
@@ -19,25 +22,15 @@ const BOOK_BLOCKS = {
   BESTSELLERS_CHILDREN: 'Children\'s Bestselling Books'
 }
 
-function getBookBlock (bookBlock) {
-  $bookBlocks.each((index, block) => {
-    if ($(block).find('h2').text() === bookBlock) {
-      $(block).find('div.book-item').each((index, book) => {
-        const title = cleanText($(book).find('h3.title').text())
-        const author = cleanText($(book).find('p.author').text())
-        const price = cleanText($(book).find('p.price').text())
-        const coverImg = $(book).find('img').attr('data-lazy')
-        const rating = $(book).find('div.rating-wrap').find('span.full-star').length + $(book).find('div.rating-wrap').find('span.half-star').length / 2
-
-        addBook(title, author, price, coverImg, rating)
-      })
-    }
-  })
-}
+// ------------------------------------------------------------ //
 
 getBookBlock(BOOK_BLOCKS.BESTSELLERS_CHILDREN)
 
-console.log(books)
+const filePath = path.join(process.cwd(), './db/books/books.json')
+
+await writeFile(filePath, JSON.stringify(books, null, 2), 'utf-8')
+
+// ------------------------------------------------------------ //
 
 // --Utils--
 // Regex to remove spaces
@@ -55,5 +48,21 @@ function addBook (title, author, price, coverImg, rating) {
     price,
     coverImg,
     rating
+  })
+}
+
+// Get book block
+function getBookBlock (bookBlock) {
+  $bookBlocks.each((index, block) => {
+    if ($(block).find('h2').text() === bookBlock) {
+      $(block).find('div.book-item').each((index, book) => {
+        const title = cleanText($(book).find('h3.title').text())
+        const author = cleanText($(book).find('p.author').text())
+        const price = cleanText($(book).find('p.price').text())
+        const coverImg = $(book).find('img').attr('data-lazy')
+        const rating = $(book).find('div.rating-wrap').find('span.full-star').length + $(book).find('div.rating-wrap').find('span.half-star').length / 2
+        addBook(title, author, price, coverImg, rating)
+      })
+    }
   })
 }
