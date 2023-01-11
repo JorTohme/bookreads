b4421f3828105c1635b2017977fb0b4c62496364
Jorge <jorlopeztohme@gmail.com>
2023-01-11 01:49:44 -0300

    Add new endpoints & refactors

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono'
 import books from '../db/books/books.json'
 
-export default {
-  async fetch (request, env, ctx) {
-    return new Response(JSON.stringify(books))
+const app = new Hono()
+
+app.get('/', (context) => context.json([
+  {
+    endpoint: '/books',
+    description: 'Returns all books'
   }
-}
+])
+)
+
+app.get('/books', (context) => context.json(books))
+
+export default app
