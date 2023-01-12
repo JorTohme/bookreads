6c86189663b392f5d45f672f65bc31f40ba9b046
Jorge <jorlopeztohme@gmail.com>
2023-01-12 19:27:20 -0300

    Update API functionalities

--- api/index.js
+++ api/index.js
@@ -1,16 +1,77 @@
 import { Hono } from 'hono'
-import books from '../db/books/books.json'
+import { cors } from 'hono/cors'
+
+import bestsellers from '../db/books/bestsellers.json'
+import bestsellersSpanish from '../db/books/bestsellersSpanish.json'
+import bestsellersManga from '../db/books/bestsellersManga.json'
 
 const app = new Hono()
+app.use(cors({ origin: '*' }))
 
 app.get('/', (context) => context.json([
   {
-    endpoint: '/books',
-    description: 'Returns all books'
+    endpoint: '/bestsellers',
+    description: 'Returns all the bestsellers',
+    parameters: {
+      name: 'date',
+      endpoint: '/bestsellers/:date',
+      description: 'Returns all the bestsellers books on the specified day',
+      format: 'DD-MM-YYYY | Months an days with one digit must NOT have a 0 before the number. Example: 12-1-2023'
+    }
+  },
+  {
+    endpoint: '/bestsellersSpanish',
+    description: 'Returns all the bestsellers in spanish',
+    parameters: {
+      name: 'date',
+      endpoint: '/bestsellersSpanish/:date',
+      format: 'DD-MM-YYYY | Months an days with one digit must NOT have a 0 before the number. Example: 12-1-2023',
+      description: 'Returns all the bestsellers in spanish on the specified day'
+    }
+  },
+  {
+    endpoint: '/bestsellersManga',
+    description: 'Returns all the manga bestsellers',
+    parameters: {
+      name: 'date',
+      endpoint: '/bestsellersManga/:date',
+      format: 'DD-MM-YYYY | Months an days with one digit must NOT have a 0 before the number. Example: 12-1-2023',
+      description: 'Returns all the manga bestsellers on the specified day'
+    }
   }
 ])
 )
 
-app.get('/books', (context) => context.json(books))
+// Bestsellers
+app.get('/bestsellers', (context) =>
+  context.json(bestsellers)
+)
+
+app.get('/bestsellers/:date', (context) => {
+  const date = context.req.param('date')
+  return bestsellers[date] ? context.json(bestsellers[date]) : context.json({ error: 'No books found for that date' })
+})
+
+// Bestsellers in Spanish
+
+app.get('/bestsellersSpanish', (context) =>
+  context.json(bestsellersSpanish)
+)
+
+app.get('/bestsellersSpanish/:date', (context) => {
+  const date = context.req.param('date')
+  return bestsellersSpanish[date] ? context.json(bestsellersSpanish[date]) : context.json({ error: 'No books found for that date' })
+})
+
+// Bestsellers Manga
+
+app.get('/bestsellersManga', (context) =>
+  context.json(bestsellersSpanish)
+)
+
+app.get('/bestsellersManga/:date', (context) => {
+  const date = context.req.param('date')
+  return bestsellersManga[date] ? context.json(bestsellersManga[date]) : context.json({ error: 'No books found for that date' })
+})
 
 export default app
